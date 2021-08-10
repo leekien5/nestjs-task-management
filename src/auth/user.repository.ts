c7da8f020b7b27880c737500db182fc7431242de
Kien Tran <kien.tt@pinetech.vn>
2021-08-10 18:21:06 +0700

    Validation Duplicate username

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
@@ -6,10 +7,20 @@ import { User } from "./user.entity";
 export class UserRepository extends Repository<User> {
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
+        
         const user = new User();
         user.username = username;
         user.password = password;
 
-        await user.save();
+        try {
+            await user.save();            
+        } catch (error) {
+            if (error.code === '23505') {
+                //Duplicate username 
+                throw new ConflictException('User name already exists');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
     }
 }
